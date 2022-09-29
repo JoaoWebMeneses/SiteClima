58c71d8eebbd4bd5fbd77ddbb2f27cd51c250c2c
JoaoWebMeneses <joaowebmenseses@gmail.com>
2022-09-29 19:27:38 -0300

    terminando de estilizar o site

---
+++ clima.js
@@ -0,0 +1,7 @@
+const timeEl = document.getElementById('time');
+const dateEl = document.getElementById('date');
+const currentWeatherItemsEl = document.getElementById('current-weather-items');
+const timezone = document.getElementById('time-zone');
+const contryEl = document.getElementById('contry');
+const weatherForecastEl = document.getElementById('weather-forecast');
+const currentTempEl = document.getElementById('current-temp');
